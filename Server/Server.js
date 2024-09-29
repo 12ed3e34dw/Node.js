d097fdd29099f4b6234f82e204a23ec66a27c007
Vladislav <166017626+12ed3e34dw@users.noreply.github.com>
2024-09-30 00:16:19 +0300

    Readme

--- Server/Server.js
+++ Server/Server.js
@@ -3,26 +3,25 @@ const path = require('path'); // Импортируем модуль path для
 const server = express(); // Инициализируем сервер
 const PORT = 5000; // Порт, на котором будет работать сервер
 
-// Middleware для обработки JSON-запросов
 server.use(express.json());
 
-// Маршрут для главной страницы
+
 server.get('/', (req, res) => {
     // Отправляем файл index.html в ответ на запрос
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Маршрут для получения данных в формате JSON
+
 server.get('/api/data', (req, res) => {
     const data = {
         message: 'Это данные из API',
         timestamp: new Date(),
     };
-    // Отправляем JSON данные в ответ
+
     res.json(data);
 });
 
-// Маршрут для обработки POST-запросов
+
 server.post('/api/data', (req, res) => {
     const receivedData = req.body; // Получаем данные из запроса
     console.log('Получены данные:', receivedData);
@@ -30,7 +29,7 @@ server.post('/api/data', (req, res) => {
     res.status(201).json({ message: 'Данные успешно получены', receivedData });
 });
 
-// Запуск сервера
+
 server.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
